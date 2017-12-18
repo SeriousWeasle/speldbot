96cdab2d38b060ff0835490e1ccd68bdbb11477b
SeriousWeasle <seriousweasle@gmail.com>
2017-12-18 18:54:54 +0100

    removing old .py file

--- speld_headline_retriever.py
+++
@@ -1,9 +0,0 @@
-from lxml import html
-import requests
-
-page = requests.get("https://speld.nl/2017/12/13/")
-tree = html.fromstring(page.content)
-
-titles = tree.xpath("//a[@rel='bookmark']/text()")
-
-print (titles)
